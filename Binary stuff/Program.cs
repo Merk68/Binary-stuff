e185eec997e7b6b6cc688ca688b55b8cb07b6342
Merk68 <marko.mark2001@gmail.com>
2021-10-07 13:43:43 +0200

    Javítgatások

--- Binary stuff/Program.cs
+++ Binary stuff/Program.cs
@@ -11,13 +11,18 @@ namespace Binary_stuff
     static void Main(string[] args)
     {
       List<int> binar = new List<int>();
+      int szamR = 0;
+      string szoveg = "";
       Console.WriteLine("Adj meg egy számot: ");
       int szam = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Milyen szamrendszerben? ");
+      szamR = Convert.ToInt32(Console.ReadLine());
       while (szam != 0)
       {
-        int maradek = szam % 2;
+        int maradek = szam % szamR;
         szam /= 2;
         Console.WriteLine($"{szam } - {maradek}");
+        szoveg = MaradekSzamjegybe(maradek) + szoveg;
         binar.Add(maradek);
         
       }
@@ -28,6 +33,18 @@ namespace Binary_stuff
         Console.Write(b);
       }
       Console.ReadLine();
+    }
+
+    private static string MaradekSzamjegybe(int maradek)
+    {
+      if (maradek < 10)
+      {
+        return maradek.ToString();
+      }
+      else
+      {
+        return ((char)(55 + maradek)).ToString();
+      }
     }
   }
 }
